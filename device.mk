ee663caa56802274a38a0a97f9369517ad895ffb
HuaLiMao-AQ <hualimao_aq@163.com>
2022-03-16 01:19:24 +0800

    atom: Support Vndk of 29 , 30 and 31
    Change-Id: I2c54cdae50c97d5cf8833129158d186c0938f34f
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

--- device.mk
+++ device.mk
@@ -13,6 +13,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 29
 
+# VNDK
+PRODUCT_EXTRA_VNDK_VERSIONS := 29 30 31
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)

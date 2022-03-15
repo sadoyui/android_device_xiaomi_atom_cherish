210bd35abf361c78265d1d92ba96d3c9bbba094b
HuaLiMao-AQ <hualimao_aq@163.com>
2022-03-16 00:53:28 +0800

    atom: Import MTK privapp permissions XML
    Change-Id: I21d8a645ac7028c64bd9b3302d413dfb4220f403
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

--- device.mk
+++ device.mk
@@ -51,3 +51,8 @@ PRODUCT_PACKAGES := \
     SystemUIOverlay \
     SettingsOverlay \
     TelephonyOverlay \
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml 
+

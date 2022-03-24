HuaLiMao-Studio/android_device_xiaomi_atom

atom: Initial skeleton tree Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com> Change-Id: I08d2c60ca1008076cacb53c4f5bb910ed2a701c3

AndroidProducts.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (C) 2022 The Kaleidoscope Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/kscope_atom.mk
+
+COMMON_LUNCH_CHOICES := \
+	kscope_atom-user \
+	kscope_atom-userdebug \
+	kscope_atom-eng

sadoyui/android_device_xiaomi_atom_cherish

AndroidProducts.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (C) 2022 The ArrowOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/arrow_atom.mk
+
+COMMON_LUNCH_CHOICES := \
+	arrow_atom-user \
+	arrow_atom-userdebug \
+	arrow_atom-eng
a7a03ec68df9cb2d1e2eb52d89a90d21fe02be18
HuaLiMao-Studio <hualimao_aq@163.com>
2022-05-01 01:48:51 +0800

    atom: Initial skeleton tree
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>
    Change-Id: I59fb4d04ef097071c2dd43e4c26c542e6b80916e

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_atom.mk
+
+COMMON_LUNCH_CHOICES := \
+	aosp_atom-user \
+	aosp_atom-userdebug \
+	aosp_atom-eng

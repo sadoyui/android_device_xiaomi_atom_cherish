14b14e2f3116bd55c181d331f82745bb9393ff6a
HuaLiMao-Studio <hualimao_aq@163.com>
2022-05-02 18:32:33 +0800

    atom: Import Mediatek permissions
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>
    Change-Id: Ie0dca312fe856903fdcfc7a894f6cf6040c7e2fc

--- device.mk
+++ device.mk
@@ -67,3 +67,7 @@ PRODUCT_BOOT_JARS += \
 # RcsService
 PRODUCT_PACKAGES += \
     RcsService
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml 

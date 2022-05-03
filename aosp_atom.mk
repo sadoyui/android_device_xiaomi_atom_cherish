f0c07ea42986d65cb2035d9d509b9b94031875b9
HuaLiMao-Studio <hualimao_aq@163.com>
2022-05-03 22:42:13 +0800

    atom: Set TARGET_BOOT_ANIMATION_RES * Set TARGET_BOOT_ANIMATION_RES to 1080
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>
    Change-Id: I44975cd4186dbce36b55251deb0cee5b7b847004

--- aosp_atom.mk
+++ aosp_atom.mk
@@ -17,6 +17,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_atom

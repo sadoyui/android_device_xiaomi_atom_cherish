55a9da4bfb0b5e70241401b0f8aaec56892bb7af
HuaLiMao-Studio <hualimao_aq@163.com>
2022-05-04 05:21:57 +0800

    atom: Modify privapp-permissions-mediatek.xml location
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>
    Change-Id: I0754ba0c82a0cc5f496546c64124e3773fe8a9e4

--- device.mk
+++ device.mk
@@ -69,4 +69,4 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml 
+    $(LOCAL_PATH)/config/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml

214f0b9c029ad386a73268e356bcbc57ddb64f70
HuaLiMao-Studio <hualimao_aq@163.com>
2022-05-05 12:54:08 +0800

    atom: fingerprint: Use fod_ui_ready sysfs about fod activity Use fod_ui_ready sysfs when user presses and releases FOD sensor.
    Change-Id: I6b247c860631527464e9d19b06b9e2f9c6d45d52
    Co-authored-by: kyasu <kyasu@users.noreply.github.com>
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

--- fingerprint/BiometricsFingerprint.h
+++ fingerprint/BiometricsFingerprint.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2021 The Android Open Source Project
+ * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,14 +15,19 @@
  * limitations under the License.
  */
 
-#pragma once
+#ifndef ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_3_BIOMETRICSFINGERPRINT_H
+#define ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_3_BIOMETRICSFINGERPRINT_H
 
 #include <android/hardware/biometrics/fingerprint/2.3/IBiometricsFingerprint.h>
+#include <android/log.h>
+#include <hardware/hardware.h>
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
+#include <log/log.h>
+#include <fcntl.h>
 #include <vendor/xiaomi/hardware/fingerprintextension/1.0/IXiaomiFingerprint.h>
-#include <vendor/xiaomi/hardware/touchfeature/1.0/ITouchFeature.h>
-#include <vendor/xiaomi/hardware/displayfeature/1.0/IDisplayFeature.h>
+
+#include "fingerprint.h"
 
 namespace android {
 namespace hardware {
@@ -30,26 +36,34 @@ namespace fingerprint {
 namespace V2_3 {
 namespace implementation {
 
-using IBiometricsFingerprint_2_1 = ::android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint;
-using ::android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprintClientCallback;
-using ::android::hardware::biometrics::fingerprint::V2_1::RequestStatus;
-using ::android::hardware::hidl_array;
-using ::android::hardware::hidl_memory;
+using ::android::sp;
 using ::android::hardware::hidl_string;
 using ::android::hardware::hidl_vec;
 using ::android::hardware::Return;
 using ::android::hardware::Void;
-using ::android::sp;
-using ::vendor::xiaomi::hardware::fingerprintextension::V1_0::IXiaomiFingerprint;
-using ::vendor::xiaomi::hardware::touchfeature::V1_0::ITouchFeature;
-using ::vendor::xiaomi::hardware::displayfeature::V1_0::IDisplayFeature;
+using ::android::hardware::biometrics::fingerprint::V2_1::FingerprintAcquiredInfo;
+using ::android::hardware::biometrics::fingerprint::V2_1::FingerprintError;
+using ::android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprintClientCallback;
+using ::android::hardware::biometrics::fingerprint::V2_1::RequestStatus;
+using ::android::hardware::biometrics::fingerprint::V2_3::IBiometricsFingerprint;
 
-struct BiometricsFingerprint : public IBiometricsFingerprint {
+using ::vendor::xiaomi::hardware::fingerprintextension::V1_0::IXiaomiFingerprint;
+
+struct BiometricsFingerprint : public IBiometricsFingerprint, public IXiaomiFingerprint {
     BiometricsFingerprint();
-    // Methods from ::V2_1::IBiometricsFingerprint follow.
-    Return<uint64_t> setNotify(const sp<IBiometricsFingerprintClientCallback>& clientCallback) override;
+    ~BiometricsFingerprint();
+
+    status_t registerAsSystemService();
+
+    // Method to wrap legacy HAL with BiometricsFingerprint class
+    static IBiometricsFingerprint* getInstance();
+
+    // Methods from ::android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint follow.
+    Return<uint64_t> setNotify(
+        const sp<IBiometricsFingerprintClientCallback>& clientCallback) override;
     Return<uint64_t> preEnroll() override;
-    Return<RequestStatus> enroll(const hidl_array<uint8_t, 69>& hat, uint32_t gid, uint32_t timeoutSec) override;
+    Return<RequestStatus> enroll(const hidl_array<uint8_t, 69>& hat, uint32_t gid,
+                                 uint32_t timeoutSec) override;
     Return<RequestStatus> postEnroll() override;
     Return<uint64_t> getAuthenticatorId() override;
     Return<RequestStatus> cancel() override;
@@ -58,16 +72,25 @@ struct BiometricsFingerprint : public IBiometricsFingerprint {
     Return<RequestStatus> setActiveGroup(uint32_t gid, const hidl_string& storePath) override;
     Return<RequestStatus> authenticate(uint64_t operationId, uint32_t gid) override;
 
-    // ::V2_3::IBiometricsFingerprint follow.
+    // Methods from ::android::hardware::biometrics::fingerprint::V2_3::IBiometricsFingerprint follow.
     Return<bool> isUdfps(uint32_t sensorId) override;
     Return<void> onFingerDown(uint32_t x, uint32_t y, float minor, float major) override;
     Return<void> onFingerUp() override;
 
-private:
-    sp<IBiometricsFingerprint_2_1> biometrics_2_1_service;
-    sp<IXiaomiFingerprint> xiaomiFingerprintService;
-    sp<ITouchFeature> touchFeatureService;
-    sp<IDisplayFeature> xiaomiDisplayFeatureService;
+    // Methods from ::vendor::xiaomi::hardware::fingerprintextension::V1_0::IXiaomiFingerprint follow.
+    Return<int32_t> extCmd(int32_t cmd, int32_t param) override;
+
+    static fingerprint_device_t* openHal();
+    static void notify(
+        const fingerprint_msg_t* msg); /* Static callback for legacy HAL implementation */
+    static Return<RequestStatus> ErrorFilter(int32_t error);
+    static FingerprintError VendorErrorFilter(int32_t error, int32_t* vendorCode);
+    static FingerprintAcquiredInfo VendorAcquiredFilter(int32_t error, int32_t* vendorCode);
+    static BiometricsFingerprint* sInstance;
+
+    std::mutex mClientCallbackMutex;
+    sp<IBiometricsFingerprintClientCallback> mClientCallback;
+    fingerprint_device_t* mDevice;
 };
 
 }  // namespace implementation
@@ -76,3 +99,5 @@ private:
 }  // namespace biometrics
 }  // namespace hardware
 }  // namespace android
+
+#endif  // ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_3_BIOMETRICSFINGERPRINT_H

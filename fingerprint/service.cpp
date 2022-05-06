6556ab2eab50b41bfd627eb335cc38a59fbde73c
HuaLiMao-Studio <hualimao_aq@163.com>
2022-05-06 23:27:18 +0800

    Revert "atom: fingerprint: Use fod_ui_ready sysfs about fod activity"
    This reverts commit 17d734cd4e69379317fa8e043b0fad94c3582dbf.

--- fingerprint/service.cpp
+++ fingerprint/service.cpp
@@ -16,61 +16,35 @@
 
 #define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service"
 
-#include <android/log.h>
+#include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
 
 #include "BiometricsFingerprint.h"
 
-// libhwbinder:
 using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
-// Generated HIDL files
+using android::hardware::biometrics::fingerprint::V2_3::IBiometricsFingerprint;
 using android::hardware::biometrics::fingerprint::V2_3::implementation::BiometricsFingerprint;
 
+using android::OK;
 using android::status_t;
 
-status_t BiometricsFingerprint::registerAsSystemService() {
-    status_t ret = 0;
-
-    ret = IBiometricsFingerprint::registerAsService();
-    if (ret != 0) {
-        ALOGE("Failed to register IBiometricsFingerprint (%d)", ret);
-        goto fail;
-    } else {
-        ALOGI("Successfully registered IBiometricsFingerprint");
-    }
-
-    ret = IXiaomiFingerprint::registerAsService();
-    if (ret != 0) {
-        ALOGE("Failed to register IXiaomiFingerprint (%d)", ret);
-        goto fail;
-    } else {
-        ALOGI("Successfully registered IXiaomiFingerprint");
-    }
-
-fail:
-    return ret;
-}
-
 int main() {
-    android::sp<BiometricsFingerprint> service = nullptr;
+    android::sp<IBiometricsFingerprint> service = new BiometricsFingerprint();
 
-    service = new BiometricsFingerprint();
-    if (service == nullptr) {
-        ALOGE("Instance of BiometricsFingerprint is null");
+    configureRpcThreadpool(1, true);
+
+    status_t status = service->registerAsService();
+    if (status != OK) {
+        LOG(ERROR) << "Cannot register Biometrics 2.3 HAL service.";
         return 1;
     }
 
-    configureRpcThreadpool(1, true /*callerWillJoin*/);
-
-    status_t status = service->registerAsSystemService();
-    if (status != android::OK) {
-        ALOGE("Cannot register service for Fingerprint HAL(%d).", status);
-        return 1;
-    }
+    LOG(INFO) << "Biometrics 2.3 HAL service ready.";
 
     joinRpcThreadpool();
 
-    return 0; // should never get here
+    LOG(ERROR) << "Biometrics 2.3 HAL service failed to join thread pool.";
+    return 1;
 }

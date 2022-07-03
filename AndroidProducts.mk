634da9d7c7aa81223423b5de76bf0e425dd20a27
sadoyui <102640681+sadoyui@users.noreply.github.com>
2022-07-03 22:21:15 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_atom.mk
+    $(LOCAL_DIR)/exthm_atom.mk
 
 COMMON_LUNCH_CHOICES := \
-	aosp_atom-user \
-	aosp_atom-userdebug \
-	aosp_atom-eng
+	exthm_atom-user \
+	exthm_atom-userdebug \
+	exthm_atom-eng

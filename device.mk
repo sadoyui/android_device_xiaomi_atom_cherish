a067faa7789a11c882a70ddc5783eeeb43e81d33
sadoyui <102640681+sadoyui@users.noreply.github.com>
2022-07-04 11:54:24 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -73,13 +73,3 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ImsServiceBase \
     libshim_vtservice
-
-# Telephony
-PRODUCT_BOOT_JARS += \
-    mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
